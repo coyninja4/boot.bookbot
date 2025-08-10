cc8512228388379ddf6a9eda9c323c2850f0b4f6
coyninja <cyoshihara4@gmail.com>
2025-08-09 22:34:16 -0700

    files split stats.py added

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def get_book_text(filepath):
-    with open(filepath) as f:
-        return f
+from stats import *
+
 def main():
-    get_book_text(books/frankenstein.txt)
+    print(f"{get_num_words("books/frankenstein.txt")} words found in the document")
+    print(number_of_characters(get_book_text("books/frankenstein.txt")))
+
+
 
 main()
---
+++ stats.py
@@ -0,0 +1,16 @@
+def get_book_text(filepath):
+    with open(filepath) as f:
+        contents = f.read()
+        return contents
+def get_num_words(filepath):
+    with open(filepath) as f:
+        contents = f.read()
+        words = contents.split()
+        return len(words)
+def number_of_characters(contents):
+    data = contents.lower()
+    characters = {}
+    for i in data:
+        try:
+            characters[i] = characters[i] + 1
+    return characters
