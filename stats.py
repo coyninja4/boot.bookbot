67bf1551ee68de5c4a06bc27d73a79d0efb44489
coyninja <cyoshihara4@gmail.com>
2025-08-11 19:47:18 -0700

    stats.py refactoring

--- stats.py
+++ stats.py
@@ -1,19 +1,17 @@
 def get_book_text(filepath):
     with open(filepath) as f:
-        contents = f.read()
-        return contents
+        return f.read() 
 def get_num_words(filepath):
     with open(filepath) as f:
         contents = f.read()
-        words = contents.split()
-        return len(words)
+        return len(contents.split())
 def number_of_characters(contents):
     data = contents.lower()
     characters = {}
     for i in data:
         present = i in characters
         if present == True:
-            characters[i] = characters[i] + 1
+            characters[i] = += 1
         else:
             characters[i] = 1
     return characters
